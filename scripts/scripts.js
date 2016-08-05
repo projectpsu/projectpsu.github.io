38456ef3e05dc0716606b8ecfee42f0819afb43f
projectpsu <pjseebald@gmail.com>
2016-08-04 22:11:57 -0400

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,10 @@
 $('#title').text("Is this a test?");
+
+$.ajax({
+	url: "https://github.com/projectpsu/projectpsu.github.io/blob/master/files/scholarshipinfo.json",
+	datatype: "json",
+	success: function(data) {
+		var json = $.parseJSON(data);
+		$('p#thisp').text(json.Name);
+	}
+});
