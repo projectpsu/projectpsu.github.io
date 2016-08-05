89496167b3dc087621f74c221b361d6147114f2a
projectpsu <pjseebald@gmail.com>
2016-08-04 22:34:17 -0400

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,6 @@
 $('#title').text("Is this a test?");
 
+/*
 $.ajax({
 	url: "https://projectpsu.github.io/files/scholarshipinfo.json",
 	datatype: "json",
@@ -10,3 +11,19 @@ $.ajax({
 		$('p#thisp').text(json.Name);
 	}
 });
+*/
+
+jQuery.getJSON("https://projectpsu.github.io/files/scholarshipinfo.json", function(data) {
+	$.each(data, function(key, val) {
+		  var items = [];
+		  $.each( data, function( key, val ) {
+		    items.push( "<li id='" + key + "'>" + val + "</li>" );
+		  });
+		 
+		  $( "<ul/>", {
+		    "class": "my-new-list",
+		    html: items.join( "" )
+		  }).appendTo( "body" );
+	})
+	
+})
